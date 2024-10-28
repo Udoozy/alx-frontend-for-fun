c746c31ea22ece7be51e4be19b8eba4d5bd3b1e5
Udoozy <agbo4samson@gmail.com>
2024-10-28 05:48:19 +0100

    markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,36 +1,67 @@
 #!/usr/bin/python3
 """
-This script reads stdin line by line and output its result
+This script reads stdin line by line and outputs HTML equivalent
 """
 
 import re
 import pathlib
 import argparse
-
+import hashlib
 
 def convert_md_to_html(input_file, output_file):
     '''
-    This converts markdown file to HTML file
+    Converts a markdown file to an HTML file
     '''
-    # This will read the content of the input file
+    # Read the content of the input file
     with open(input_file, encoding='utf-8') as w:
         mkdwn_content = w.readlines()
 
     html_file = []
-    for line in mkdwn_content:
-        # Check if the line is a heading line
-        match = re.match(r'(#){1,6} (.*)', line)
-        if match:
-            # get th header level
-            h_level = len(match.group(1))
-            # Get the contents of the heading
-            h_content = match.group(2)
-            # The append the HTML equivalent of the heading
-            html_file.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
-        else:
-            html_file.append(line)
+    in_list = False
 
-            # Write the HTML content to the stdout
+    for line in mkdwn_content:
+        line = line.strip()
+
+        #heading convertion
+        match = re.match(r'^(#{1,6}) (.*)', line)
+        if match:
+            h_level = len(match.group(1))
+            h_content = match.group(2)
+            html_file.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
+            continue
+
+        # List items conversion
+        if line.startswith('- '):
+            if not in_list:
+                html_file.append('<ul>\n')
+                in_list = True
+            list_content = line[2:]
+            #Bold formatting and italic
+            list_content = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', list_content)
+            list_content = re.sub(r'__(.*?)__', r'<em>\1</em>', list_content)
+            html_file.append(f'<li>{list_content}</li>\n')
+            continue
+        elif in_list:
+            html_file.append('</ul>\n')
+            in_list = False
+
+        #Paragraph and line break
+        if line:
+            #Bold and italic
+            line = re.sub(r'\*\*(.*?)\*\*', r'<b>\1</b>', line)
+            line = re.sub(r'__(.*?)__', r'<em>\1</em>', line)
+            #applying replacement
+            line = re.sub(r'\[\[private\]\]', hashlib.md5(b'private').hexdigest(), line)
+            #reming parathesis
+            line = re.sub(r'\(\((.*?)\)\)', r'\1', line)
+
+            html_file.append(f'<p>{line}</p>\n')
+
+    #close every list tag
+    if in_list:
+        html_file.append('</ul>\n')
+
+    #writing HTML contents to the output file
     with open(output_file, 'w', encoding='utf-8') as f:
         f.writelines(html_file)
 
@@ -41,7 +72,7 @@ if __name__ == '__main__':
     parser.add_argument('output_file', help='path to output HTML file')
     args = parser.parse_args()
 
-    # Check if the input file exists
+    # Check if the input file exist
     input_path = pathlib.Path(args.input_file)
     if not input_path.is_file():
         print(f'Missing {input_path}', file=sys.stderr)
